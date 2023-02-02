493fd4bab3a1f0620345497ea033ec9994fd85d9
shenlingzhi <2087789639@qq.com>
2023-02-02 23:50:48 +0800

    commit

--- chapter1/main.c
+++ chapter1/main.c
@@ -2,5 +2,12 @@
 
 int main() {
     printf("The first program of data structure\n");
+    int a[9];
+    for (int i = 0; i < 9; i++) {
+        a[i] = i;
+    }
+    for (int i = 0; i < 9; i++) {
+        printf("%d ", a[i]);
+    }
     return 0;
 }
